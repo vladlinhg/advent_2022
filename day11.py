4b1719cc46b5df8f2096bc5b975c6184ae5948d5
vladlinhg <victor.lin.hg@gmail.com>
2022-12-16 15:04:36 -0800

    day11 in progress

--- day11.py
+++ day11.py
@@ -9,7 +9,7 @@ class Item:
             else:
                 self.worry *= int(instruction[1])
         else:
-            self.worry += instruction[0]
+            self.worry += int(instruction[1])
 
     def worry_test(self, instruction):
         self.worry /= 3
@@ -17,16 +17,37 @@ class Item:
             return True
         else:
             return False 
+    def __str__(self) -> str:
+        return str(self.worry)
 
 
 class Monkey:
     def __init__(self, instruction):
-        self.id = int(instruction["id"])
-        self.op = instruction["op"]
-        self.test = instruction["test"]
-        self.pas = instruction["pas"]
-        self.fai = instruction["fai"]
-        self.items = instruction["items"]
+        for line in instruction:
+            line = line.strip().split()
+            for i, word in enumerate(line):
+                line[i] = word.strip(":,")
+            if line[0] == "Monkey":
+                self.id = int(line[1])
+            elif line[0] == "Starting":
+                self.items = []
+                for i in line:
+                    if i == "Starting" or i == "items":
+                        continue
+                    else:
+                        self.items.append(Item(int(i)))
+            elif line[0] == "Operation":
+                if line[5] == 'old':
+                    self.op = [line[4], line[5]]
+                else:
+                    self.op = [line[4], int(line[5])]
+            elif line[0] == "Test":
+                self.test = int(line[3])
+            elif line[0] == "If":
+                if line[1] == "true":
+                    self.pas = int(line[5])
+                else:
+                    self.fai = int(line[5])
         self.insp = 0
 
 
@@ -38,9 +59,65 @@ class Monkey:
         for i in self.items:
             i.worry_change(self.op)
     
-    def test_item(self):
+    def test_item(self, monkeys):
+        for monkey in monkeys:
+            if monkey.id == self.pas:
+                pas = monkey
+            if monkey.id == self.fai:
+                fai = monkey
         for i in self.items:
             if i.worry_test(self.test):
-                self.pass_item(i, Monkey.objects.get(id=self.pas))
+                self.pass_item(i, pas)
             else:
-                self.pass_item(i, Monkey.objects.get(id=self.fai))
+                self.pass_item(i, fai)
+            self.insp += 1
+    
+    def process(self, monkeys):
+        self.play_item()
+        self.test_item(monkeys)
+    
+    def print(self):
+        print(self.id)
+        print(self.items)
+        print(self.op)
+        print(self.test)
+        print(self.pas)
+        print(self.fai)
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+        
+
+
+with open('day11_t.txt') as f:
+    lines = f.readlines()
+
+instructions = []
+tmp = []
+for line in lines:
+    if line == '\n':
+          tmp = []
+    elif line.strip()[:8] == 'If false':
+        tmp.append(line.strip())
+        instructions.append(tmp)
+    else:
+        tmp.append(line.strip())
+
+
+monkeys = []
+for i in instructions:
+    monkey = Monkey(i)
+    monkeys.append(monkey)
+
+for i in monkeys:
+    i.print()
+
+for i in range(20):
+    for monkey in monkeys:
+        monkey.process(monkeys)
+
+log = []
+
+for monkey in monkeys:
+    log.append((monkey.id, monkey.insp))
+
+print(log)
